523d9e7e4412735011cfee17be52fa864e3611b3
ArchSeraphin <trinity.informatique@gmail.com>
2026-05-19 16:50:44 +0200

    chore: configure PHPUnit with unit/integration suites

---
+++ tests/Unit/SentinelTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Trinity\Booking\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class SentinelTest extends TestCase
+{
+    public function test_runner_is_alive(): void
+    {
+        self::assertTrue(true);
+    }
+}
