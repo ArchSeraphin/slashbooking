94b5a3425d2a2edd7251881f482a7994b4b7f1d2
ArchSeraphin <trinity.informatique@gmail.com>
2026-05-19 16:52:36 +0200

    chore: configure PHPStan level 8 and WordPress coding standards

--- tests/Unit/SentinelTest.php
+++ tests/Unit/SentinelTest.php
@@ -5,10 +5,9 @@ namespace Trinity\Booking\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-final class SentinelTest extends TestCase
-{
-    public function test_runner_is_alive(): void
-    {
-        self::assertTrue(true);
-    }
+final class SentinelTest extends TestCase {
+
+	public function test_runner_is_alive(): void {
+		self::assertTrue( true );
+	}
 }
